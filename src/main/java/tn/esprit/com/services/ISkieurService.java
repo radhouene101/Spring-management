5818ecc9711a054412372d411a1ced675f0739dc
blast <fberrzig@gmail.com>
2024-02-23 14:57:31 +0100

    added more method to skieur interface

--- src/main/java/tn/esprit/com/services/ISkieurService.java
+++ src/main/java/tn/esprit/com/services/ISkieurService.java
@@ -3,10 +3,15 @@ package tn.esprit.com.services;
 
 import tn.esprit.com.entities.Skieur;
 
+import java.util.List;
+
 public interface ISkieurService {
 
     Skieur addSkieur(Skieur ski);
     Skieur updateSkieur(Skieur ski);
     void removeSkieur(Skieur ski);
+    void removeSkieurById(Integer ski);
+    Skieur retrieveSkieur(Integer idSkieur);
+    List<Skieur> retrieveAllSKieyr();
 
 }
