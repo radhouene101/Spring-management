7a8388b37cbf57d820fb98d4384733fae13ad6f2
blast <fberrzig@gmail.com>
2024-02-27 21:43:48 +0100

    added and tested Moniteur controller

---
+++ src/main/java/tn/esprit/com/controllers/MoniteurController.java
@@ -0,0 +1,35 @@
+package tn.esprit.com.controllers;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.com.entities.Moniteur;
+import tn.esprit.com.services.IMoniteurService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/moniteur")
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PUBLIC)
+public class MoniteurController {
+    IMoniteurService moniteurService;
+    @GetMapping("/getAll")
+    public List<Moniteur> getAllMoniteurs(){
+        return moniteurService.retrieveAllMoniteurs();
+    }
+    @PostMapping("/update")
+    public Moniteur updateMoniteur(@RequestBody Moniteur mo){
+        return moniteurService.updateMoniteur(mo);
+    }
+    @PutMapping("/add")
+    public Moniteur addMoniteur(@RequestBody Moniteur mo){
+        return moniteurService.addMoniteur(mo);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void DeleteMoniteur(@PathVariable Integer id){
+        moniteurService.removeMoniteurById(id);
+    }
+}
