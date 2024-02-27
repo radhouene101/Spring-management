20e8c38a0a786ed865a43cdab4276c20b0f4532b
blast <fberrzig@gmail.com>
2024-02-27 20:56:31 +0100

    added and tested Inscription controller

--- src/main/java/tn/esprit/com/controllers/InscriptionController.java
+++ src/main/java/tn/esprit/com/controllers/InscriptionController.java
@@ -1,4 +1,37 @@
 package tn.esprit.com.controllers;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.com.entities.Inscription;
+import tn.esprit.com.services.IInscriptionService;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@FieldDefaults(level = AccessLevel.PUBLIC)
+@RequestMapping("api/inscription")
 public class InscriptionController {
+
+    IInscriptionService iInscriptionService;
+    @GetMapping("/getAll")
+    public Iterable<Inscription> getAllInscriptions(){
+        return iInscriptionService.retrieveAllInscriptions();
+    }
+    @PutMapping("/add")
+    public Inscription addInscription(@RequestBody Inscription inscription){
+        return iInscriptionService.addInscription(inscription);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteInscription(@PathVariable Integer id){
+        iInscriptionService.removeInscriptionById(id);
+    }
+    @PostMapping("/update")
+    public Inscription updateInscription(@RequestBody Inscription inscription){
+        return iInscriptionService.updateInscription(inscription);
+    }
+
+
 }
