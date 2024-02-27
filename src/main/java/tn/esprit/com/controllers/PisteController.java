28fa50d2bc424e9a7a80b09dcad9381eb0206127
blast <fberrzig@gmail.com>
2024-02-27 21:30:40 +0100

    added and tested Piste controller

--- src/main/java/tn/esprit/com/controllers/PisteController.java
+++ src/main/java/tn/esprit/com/controllers/PisteController.java
@@ -1,4 +1,34 @@
 package tn.esprit.com.controllers;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.com.entities.Piste;
+import tn.esprit.com.services.IPisteService;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RequestMapping("api/piste")
+@FieldDefaults(level = AccessLevel.PUBLIC)
+@RestController
 public class PisteController {
+    IPisteService iPisteService;
+    @GetMapping("/getAll")
+    public List<Piste> getAllPistes(){
+        return iPisteService.getAllPistes();
+    }
+    @PutMapping("/add")
+    public Piste addPiste(@RequestBody  Piste piste){
+        return iPisteService.addPiste(piste);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deletePiste(@PathVariable Integer id){
+        iPisteService.removePisteById(id);
+    }
+    @PostMapping("/update")
+    public Piste updatePiste(@RequestBody Piste piste){
+        return iPisteService.updatePiste(piste);
+    }
 }
